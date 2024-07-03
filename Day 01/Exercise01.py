bf378108a9c820293df832523cfd4ef7e65cc5cb
Muhammad Bilal Ashiq <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2024-07-03 15:00:52 +0500

    Create Exercise01.py

---
+++ Day 01/Exercise01.py
@@ -0,0 +1,43 @@
+# ==============================  Task 01 ========================
+# Import this module (webbrowser). And perform any one example. 
+
+# ==============================  Task 02 ========================
+# import this module "matplotlib" and run any code as example
+
+# ==============================  Task 03 ========================
+# Write a code for multiple line output and multiple line comment. Also write the shortcut for single line comment
+
+
+
+
+#----------- > Sample for Solution of task 01
+import webbrowser
+
+# Open Wikipedia
+url = "https://www.linkedin.com/in/bilal-ashiq/"
+webbrowser.open(url)
+
+# Open a specific YouTube video
+video_url = "https://www.youtube.com/shorts/UgS4K3Nae4s"  # Replace with a real video URL
+webbrowser.open(video_url)
+
+
+# ----------- > Sample for Solution of task 02 (You have to write different code)
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 5, 3]
+
+# Create a line plot
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel("Bilal-axis")
+plt.ylabel("Kamal-axis")
+plt.title("Line Plot Example")
+
+# Show the plot
+plt.show()
+
+
