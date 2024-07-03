98bc521f98a9c3a903051064d65b6fe5337019df
Muhammad Bilal Ashiq <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2024-07-03 14:48:14 +0500

    Create Codes.py

---
+++ Day 01/Codes.py
@@ -0,0 +1,49 @@
+# =================================================================================================== #
+# ==================================                            ===================================== #
+# ==================================       Python Day 01        ===================================== #
+# ==================================     Muhammad Bilal Ashiq   ===================================== #
+# ==================================                            ===================================== #
+# =================================================================================================== #
+
+
+#  ------------------------------------   Printing  --------------------------------------------
+
+# print one line
+print ("Muhammad Bilal Ashiq   1 line print")     
+
+# print multiple line
+print ("""
+To print
+       more than 1 line
+            yes it prints
+""")
+
+
+#  -------------------------------------   MODULES  -----------------------------------------------
+
+#  ----  Just sample how to download and import any module -----
+# Open termianl write -->      pip install module_name ===>>>> pip install pyjokes
+# import module_name.
+
+
+import pyttsx3               # pip install pyttsx by opening terminal 
+engine = pyttsx3.init()
+engine.say("Hey I am Muhammad Beelal Aashiq")    # robote will call my name 
+engine.runAndWait()
+
+
+#  ------------------------------------   Add Comments  --------------------------------------------
+
+
+# --->> one line comment   or ctrl + /
+
+'''
+multiple line
+More tha 1 line comment
+Bilal
+'''
+
+#  ------------------------------------  REPL    --------------------------------------------
+
+# open  terminal or cmd on windows and right python then enter.. then run your code there.. like (4+5 enter ) print("Hello world")
+
